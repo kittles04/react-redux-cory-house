ad4ca0ecefba41b05f3e805a534eb91d84995105
kerri <kwalker@wideorbit.com>
2021-01-10 21:52:30 -0500

    [react-redux-PS]: Dev environment setup, react router and navigation

---
+++ src/components/PageNotFound.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+const PageNotFound = () => <h1>The Page was not found! Where is it?</h1>;
+
+export default PageNotFound;
---
+++ webpack.config.dev.js
@@ -0,0 +1,49 @@
+const webpack = require("webpack");
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+process.env.NODE_ENV = "development";
+
+module.exports = {
+  mode: "development",
+  target: "web",
+  devtool: "cheap-module-source-map",
+  // entrypoint: "./src/index",
+  output: {
+    path: path.resolve(__dirname, "build"),
+    publicPath: "/",
+    filename: "bundle.js",
+  },
+  devServer: {
+    stats: "minimal",
+    overlay: true,
+    historyApiFallback: true,
+    disableHostCheck: true,
+    headers: { "Access-Control-Allow-Origin": "*" },
+    https: false,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "src/index.html",
+      favicon: "src/favicon.ico",
+    }),
+    new webpack.LoaderOptionsPlugin({
+      options: {
+        entrypoint: "./src/index",
+      },
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ["babel-loader", "eslint-loader"],
+      },
+      {
+        test: /(\.css)$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+};
